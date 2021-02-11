05e1a15ecf5afb17cdddb72892dd93c7f7b75831
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-02-11 10:14:20 +0300

    Updated Database and DbScripts

--- backend/database/dbScripts.sql
+++ backend/database/dbScripts.sql
@@ -1,9 +1,19 @@
+-- Drop tables
+Drop table reviews;
+Drop table images;
+Drop table products;
+Drop table categories;
+Drop table sellers;
+
+-- Create tables
 CREATE TABLE categories(
 	id serial NOT NULL,
 	name VARCHAR(30),
 	description VARCHAR(400),
 	created_date TIMESTAMP,
 	created_by VARCHAR(30),
+	updated_date TIMESTAMP,
+	updated_by VARCHAR(30),
 	is_active bool,
 	PRIMARY KEY(id)
 );
@@ -13,13 +23,13 @@ CREATE TABLE sellers(
 	name VARCHAR(50),
 	description VARCHAR(400),
 	address VARCHAR(200),
-	created_on TIMESTAMP,
+	created_date TIMESTAMP,
 	created_by VARCHAR(30),
+	updated_date TIMESTAMP,
+	updated_by VARCHAR(30),
 	is_active bool,
 	PRIMARY KEY (id)
 );
-
-
 
 CREATE TABLE products(
 	id serial NOT NULL,
@@ -32,11 +42,12 @@ CREATE TABLE products(
     stock_amount int,
 	created_date TIMESTAMP,
 	created_by VARCHAR(30),
+	updated_date TIMESTAMP,
+	updated_by VARCHAR(30),
 	is_active bool,
 	PRIMARY KEY (id),
 	FOREIGN KEY (category_id) REFERENCES categories(id),
 	FOREIGN KEY (seller_id) REFERENCES sellers(id)
-
 );
 
 CREATE TABLE reviews(
@@ -47,6 +58,8 @@ CREATE TABLE reviews(
     product_id int,
 	created_date TIMESTAMP,
 	created_by VARCHAR(30),
+	updated_date TIMESTAMP,
+	updated_by VARCHAR(30),
 	is_active bool,
 	PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES products(id)
@@ -59,12 +72,14 @@ CREATE TABLE images(
     product_id int,
 	created_date TIMESTAMP,
 	created_by VARCHAR(30),
+	updated_date TIMESTAMP,
+	updated_by VARCHAR(30),
 	is_active bool,
 	PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
--- Insert into Categories Table
+-- Insert data to tables
 INSERT INTO categories(
 	name, description, created_date, created_by, is_active)
 	VALUES 
