8c1b299010bee47d22ea81bf78a8351845e79490
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-02-11 10:41:56 +0300

    Admin Routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const { getAllProducts, createProduct, getSingleProductById, updateProductById } = require("../controllers/productController");
 
 router.route("/products").get(getAllProducts);
-router.route("/products/new").post(createProduct);
 router.route("/products/:id").get(getSingleProductById);
-router.route("/products/:id").put(updateProductById);
+router.route("/admin/products/new").post(createProduct);
+router.route("/admin/products/:id").put(updateProductById);
 
 module.exports = router;
