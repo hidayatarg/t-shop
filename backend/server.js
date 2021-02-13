5fc6f97d1cf8c220e4ba6f681e5ab43dda0e555c
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-02-13 04:09:53 +0300

    Unhandled Promise Rejection [when DB is not Reached]

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
-const app = require("./app");
-const dotenv = require("dotenv");
+const app = require('./app');
+const dotenv = require('dotenv');
 
 const port = process.env.PORT || 3000;
 
 // setting config file
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
-app.listen(port, () => {
-  console.log(
-    `server started on PORT: ${port} in ${process.env.NODE_ENV} mode`
-  );
+var server = app.listen(port, () => {
+  console.log(`server started on PORT: ${port} in ${process.env.NODE_ENV} mode`);
+});
+
+// UnhandledRejection
+process.on('unhandledRejection', err => {
+  console.log(`Error: ${err.message}`);
+  console.log('Shutting down the server due to Unhandled Promise Rejection');
+  server.close(() => {
+    process.exit(1);
+  })
 });
