7fa3cdcdef60e04e7f7486255b0b25d5570cd293
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-03-21 17:41:57 +0300

    AuthorizeRoles Middleware Added to the Product Routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -11,15 +11,15 @@ const {
 	deleteProductById,
 } = require('../controllers/productController');
 
-router
-	.route('/products')
-	.get(isAuthenticatedUser, authorizeRoles('admin'), getAllProducts);
+router.route('/products').get(getAllProducts);
 router.route('/products/:id').get(getSingleProductById);
-router.route('/admin/products/new').post(isAuthenticatedUser, createProduct);
+router
+	.route('/admin/products/new')
+	.post(isAuthenticatedUser, authorizeRoles('admin'), createProduct);
 
 router
 	.route('/admin/products/:id')
-	.put(isAuthenticatedUser, updateProductById)
-	.delete(isAuthenticatedUser, deleteProductById);
+	.put(isAuthenticatedUser, authorizeRoles('admin'), updateProductById)
+	.delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProductById);
 
 module.exports = router;
